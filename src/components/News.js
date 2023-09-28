a6e83198a1325efd9f4e8eb95e14e418e53d1760
Tushar <mathurtushar2002@gmail.com>
2023-09-28 19:35:45 +0530

    setting up next button

--- src/components/News.js
+++ src/components/News.js
@@ -7,50 +7,55 @@ export class News extends Component {
         this.state = {
             articles: [],
             loading: false,
-            page:1
+            page: 1,
+
         }
     }
     async componentDidMount() {
         // fetching data from the API
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=58ff87b9591c471bae09bc72398fced0&page=1";
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=58ff87b9591c471bae09bc72398fced0&page=1&pageSize=20";
         let data = await fetch(url);
         //json coverted to parsed data
         let parsedData = await data.json()
         console.log(parsedData);
         //now the getting data from the API is equal to articles so that on changing API the component data will also changed
-        this.setState({ articles: parsedData.articles })
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
     }
     //for the next and prev. page
     handleprevclick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=58ff87b9591c471bae09bc72398fced0&page=${this.state.page - 1}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=58ff87b9591c471bae09bc72398fced0&page=${this.state.page - 1}&pageSize=20`;
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
         this.setState({
-            page: this.state.page-1,
+            page: this.state.page - 1,
             articles: parsedData.articles
         })
     }
 
     handlenextclick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=58ff87b9591c471bae09bc72398fced0&page=${this.state.page + 1}`;
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        console.log(parsedData);
-        this.setState({
-            page: this.state.page+1,
-            articles: parsedData.articles
-        })
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+        }
+        else {
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=58ff87b9591c471bae09bc72398fced0&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json()
+            console.log(parsedData);
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
     }
     render() {
         return (
             <div className="container my-3">
-                <h2>Top Headlines</h2>
+                <h2 className='text-center'>Top Headlines</h2>
                 <div className='row'>
                     {this.state.articles.map((element) => {
                         return <div className='col-md-3' key={element.url}>
                             {/* here we are using ternary operator so that if value is null it can return default */}
-                            <NewsItems title={element ? element.title : ""}
+                            <NewsItems title={element ? element.title.slice(0, 26) : ""}
                                 description={element ? element.description : ""}
                                 imageUrl={element.urlToImage}
                                 newsUrl={element.url} />
