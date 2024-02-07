62056c2d38205fa04399b700d35e38c2e81a145d
Tushar <tushardev2002@gmail.com>
2024-02-07 20:47:07 +0530

    edit navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,7 +17,7 @@ export class NavBar extends Component {
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/about">About</a>
+          <a className="nav-link" href="/about">Sports</a>
         </li>
       </ul>
     </div>
